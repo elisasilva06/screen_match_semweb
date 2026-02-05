791a497ddb8d8c2a1681982d70c370f477bd73b6
Ana Elisa <ana.eosilva@aluno.obompastor.com.br>
2026-02-05 20:50:17 -0300

    atualizando projeto

--- src/main/java/br/com/elisas/screen_match/ScreenMatchApplication.java
+++ src/main/java/br/com/elisas/screen_match/ScreenMatchApplication.java
@@ -1,8 +1,7 @@
 package br.com.elisas.screen_match;
 
-import br.com.elisas.screen_match.model.DadosSerie;
-import br.com.elisas.screen_match.service.ConsumoAPI;
-import br.com.elisas.screen_match.service.ConverteDados;
+
+import br.com.elisas.screen_match.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +15,11 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-        var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=487226a7");
-        System.out.println(json);
-        //json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
-        //System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+        Principal principal = new Principal();
+        principal.exibeMenu();
+
+
+
+
 	}
 }
